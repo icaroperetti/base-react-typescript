8de0733532772396ba4a0baeeed96c0d3d90ad40
icaroperetti <icaroperetti50@gmail.com>
2021-11-03 15:45:37 -0300

    base for react application with typescript

--- src/application/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header'
-export { default as Sidebar } from './Sidebar'
-export { default as HeaderTitle } from './Title'
-export { default as MenuButton } from './MenuButton'
-export { default as ContentApp } from './ContentApp'
--- src/application/pages/App/index.tsx
+++ src/application/pages/App/index.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
-import { Header, Sidebar, ContentApp } from '../../components'
-import { DashboardProvider } from '../../hooks'
 
 import useStyles from './styles'
 
 const App: React.FC = () => {
   const classes = useStyles()
 
-  return (
-    <div className={classes.root}>
-      <DashboardProvider>
-        <Header />
-        <Sidebar />
-      </DashboardProvider>
-      <ContentApp />
-    </div>
-  )
+  return <div className={classes.root}>Hello World</div>
 }
 
 export default App
